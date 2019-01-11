4249b94f12deaa7220429877bdcc696df2141dc0
NestZ <bobonono2009@windowslive.com>
2019-01-11 23:04:18 +0700

    2nd Assingment Finished!!

---
+++ Ex03_2.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+using namespace std;
+
+int main(){
+    int i = 3,sum = 0;
+    while(i <= 300){
+        sum = sum + i*i;
+        i += 3;
+    }
+    printf("%d",sum);
+
+    return 0;
+}
