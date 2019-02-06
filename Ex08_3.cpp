796b4b93a0dfb2f51a07b45c179fe708aa12ec27
NestZ <bobonono2009@windowslive.com>
2019-02-06 20:41:45 +0700

    fixed Ex08_3

--- Ex08_3.cpp
+++ Ex08_3.cpp
@@ -2,12 +2,23 @@
 using namespace std;
 
 int main(){
-  int input = 1;
+  int input = 1,min = 0;
+  bool start = false;
 
   while(input > 0){
     cout << "Input number : ";
     cin >> input;
+    if(!start){
+      if(input<=0){
+        cout << "N/A";
+        break;
+      }
+      else min = input;
+    }
+    else if(input < min && input != 0)min = input;
+    start = true;
   }
+  if(min != 0)cout << "Minimum = " << min << "\n";
 
   return 0;
 }
